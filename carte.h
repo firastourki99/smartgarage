30050e5aa965c361913e114f8b8b61f6b5ef2f92
firas <firastourki24@gmail.com>
2021-12-06 14:08:47 +0100

    integration

---
+++ carte.h
@@ -0,0 +1,32 @@
+#ifndef CARTE_H
+#define CARTE_H
+#include"connection.h"
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QtDebug>
+#include <QSqlQueryModel>
+
+
+class carte
+{
+public:
+    carte();
+    carte(QString,QString);
+
+
+    bool ajouter();
+    QSqlQueryModel* afficher();
+    bool supprimer();
+    bool update();
+    QString  recherchee();
+
+    //sett
+    void setcin(QString);
+
+private:
+    QString point ;
+    QString cin;
+};
+
+#endif // CARTE_H
